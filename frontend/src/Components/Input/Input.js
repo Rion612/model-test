079b9522a9bb428c65cd27b05e21d8fd8080b5ca
Md. Nazmul Hasan <mdnazmul612@gmail.com>
2021-06-19 17:48:43 +0600

    commit

---
+++ frontend/src/Components/Input/Input.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Form} from 'react-bootstrap'
+
+function Input(props) {
+    return (
+        <>
+            <Form>
+                <Form.Group>
+                    <Form.Label>{props.label}</Form.Label>
+                    <Form.Control
+                        type={props.type}
+                        placeholder={props.placeholder}
+                        value={props.value}
+                        onChange={props.onChange}
+                        name={props.name}
+                        icon={props.icon}
+                    />
+                </Form.Group>
+            </Form>
+
+        </>
+    )
+}
+export default Input
--- frontend/src/Conatiner/Course/Course.js
+++ frontend/src/Conatiner/Course/Course.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../Components/Layout/Layout';
-import { Container, Row, Col, Button,Table } from 'react-bootstrap';
+import { Container, Row, Col, Button, Table, Modal } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import './course.css';
 import { ImPriceTag } from 'react-icons/im';
 import { BiTimeFive } from 'react-icons/bi';
 import { FaKey } from 'react-icons/fa';
+import Input from '../../Components/Input/Input';
 const Course = (props) => {
     const slug = props.match.params.slug;
     const courses = useSelector(state => state.course.courses);
     const item = courses.find(x => x.slug === slug);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div>
             <Layout>
@@ -38,23 +43,23 @@ const Course = (props) => {
                                 <Table bordered>
                                     <tbody>
                                         <tr>
-                                            <td><BiTimeFive/> Length</td>
+                                            <td><BiTimeFive /> Length</td>
                                             <td>4 weeks</td>
                                         </tr>
                                         <tr>
-                                            <td><FaKey/> Total No. of Model-tests</td>
+                                            <td><FaKey /> Total No. of Model-tests</td>
                                             <td>25</td>
                                         </tr>
                                         <tr>
-                                            <td><ImPriceTag/> Price</td>
+                                            <td><ImPriceTag /> Price</td>
                                             <td>{item?.price}</td>
                                         </tr>
-                                       
+
                                     </tbody>
                                 </Table>
                             </Col>
                             <Col md={6} style={{ marginTop: '50px' }}>
-                                <Button variant="danger" size="lg" block>
+                                <Button variant="danger" size="lg" block onClick={handleShow}>
                                     Enrollment
                                 </Button>
 
@@ -63,6 +68,68 @@ const Course = (props) => {
                     </Container>
                 </div>
             </Layout>
+            <Modal show={show} onHide={handleClose} style={{ marginTop: '40px' }} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Enrollment And Payment</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+
+                        <Col><label className="font-weight-bold">Payment Method :</label></Col>
+                        <Col>
+                            <img
+                                src={process.env.PUBLIC_URL + "/bkash_logo.png"}
+                                alt="bkash logo"
+                                height="60px"
+                                width="200px" />
+                        </Col>
+                    </Row>
+                    <hr />
+                    <Row>
+
+                        <Col>
+                            
+                            <br />
+                            <img
+                                src={process.env.PUBLIC_URL + "/bkash-system.png"}
+                                alt="bkash logo"
+                                height="400px"
+                                width="100%" />
+                        </Col>
+                    </Row>
+                    <hr />
+                    <Row className="mt-3" style={{fontSize:'18px'}}>
+
+                        <Col>
+                            <p className="text-danger">If you have completed your payment,
+                                you are now requested to submit your transaction
+                                ID in the section below.
+                            </p>
+                        </Col>
+                    </Row>
+                    <hr />
+                    <Row className="mt-3">
+
+                        <Col>
+                        <label className="font-weight-bold">Payment transaction Id:</label>
+                            <Input 
+                                type="text"
+                                placeholder="Transaction id"
+
+                            />
+                        </Col>
+                    </Row>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
         </div>
     );
